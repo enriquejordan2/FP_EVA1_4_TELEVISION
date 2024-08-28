492d9ae8272ccf0ebca61949137670907f781482
enriquejordan2 <L24550342@chihuahua2.tecnm.mx>
2024-08-28 16:12:17 -0600

    Add files via upload

---
+++ eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_television;
+
+/**
+ *
+ * @author Jose Enrique
+ */
+public class EVA1_4_TELEVISION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int pulgadas;
+        String resolucion;
+        double precio;
+        String tecno;
+        
+        //Asignar valores
+        marca = "LG";
+        modelo = "AI ThinQ";
+        pulgadas = 50;
+        resolucion = "4k";
+        precio = 7999.0;
+        tecno = "LED";
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(pulgadas);
+       System.out.println(resolucion);
+       System.out.println(precio);
+       System.out.println(tecno);
+        
+    }
+    
+}
